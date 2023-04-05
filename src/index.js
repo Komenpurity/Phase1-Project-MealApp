97efc12fa6895043923463294b1e96cb6086bc59
Komenpurity <jepkorirpurity8@gmail.com>
2023-04-05 12:38:32 +0300

    display food starting with d

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
     document.addEventListener("DOMContentLoaded",() => {
-
-    // fetch("https://www.themealdb.com/api/json/v1/1/filter.php?i=chicken")
     fetch("https://www.themealdb.com/api/json/v1/1/categories.php") 
         .then(response => response.json())
         .then( data => {
-            console.log(data) 
+           // console.log(data) 
             displayAll(data)
         })
 
@@ -15,7 +13,7 @@
     //display all 
     function displayAll(data){
         data.categories.forEach(element => {
-            console.log(element) 
+           // console.log(element) 
 
             displayCategories.innerHTML += `
                     <h5 ${element.idCategory}>${element.strCategory}</h5>
@@ -23,4 +21,27 @@
                     <p>${element.strCategoryDescription}</p>
             `
         });
+    }
+
+
+    fetch("https://www.themealdb.com/api/json/v1/1/search.php?f=d") 
+        .then(response => response.json())
+        .then( data => {
+            console.log(data) 
+            displayFoodDetails(data)
+        })
+
+     let foodDetails = document.getElementById("food-details")
+
+    //diplay food details starting with letter C
+    function displayFoodDetails(data){
+        data.meals.forEach(element => {
+            foodDetails.innerHTML += `
+                    <img src= ${element.strMealThumb} class="img-fluid card-img-top"/>
+                    <h5 class="card-title" data-id = ${element.idMeal}>${element.strMeal}</h5>
+                    <p class="card-text">${element.strCategory}</p>
+                    <a href="#" class="btn btn-primary">Recipe</a>
+            `
+
+        })
     }

5c422d118c4e58f770e1b284659ae91c0b2ed047
Komenpurity <jepkorirpurity8@gmail.com>
2023-04-06 14:19:45 +0300

    display recipe is working

--- src/index.js
+++ src/index.js
@@ -49,19 +49,42 @@ const FULL_HEART = '♥'
             <div class="m-4">
                 <img src= ${element.strMealThumb} id="img" class="img-fluid card-img-top"/>
                 <h5 class="card-title" id = "title"  data-id = ${element.idMeal}>${element.strMeal}</h5>
-                <p id= "element-category"  class="card-text">${element.strCategory} </p>
-                <button data-id = ${element.idMeal}  id = "recipe" class="btn btn-success">Recipe</button>
+                <p id= "element-category"  class="card-text">Category: ${element.strCategory} </p>
+                <div id="recipe"  style="display: none;">
+                    <h5 class= "display-5 font-weight-bold">The Recipe</h5>
+                    <p>Ingredients: ${element.strIngredient1}, ${element.strIngredient2},  ${element.strIngredient3}, ${element.strIngredient4},  ${element.strIngredient5},  ${element.strIngredient6},${element.strIngredient7},  ${element.strIngredient8}, ${element.strIngredient9} ,  ${element.strIngredient10}, ${element.strIngredient11}, ${element.strIngredient12},${element.strIngredient13}         </p>
+                    <h6 class= "display-6 font-weight-bold"> Instructions </h6>
+                    <p>${element.strInstructions}</p>
+                    <p>Measures: ${element.strMeasure1},${element.strMeasure2},${element.strMeasure3},${element.strMeasure4},${element.strMeasure5}, ${element.strMeasure6},${element.strMeasure7},${element.strMeasure8},${element.strMeasure9},${element.strMeasure10},${element.strMeasure11},${element.strMeasure12},</p>
+                    <button id="hide-recipe">X</button>
+                </div>
+                <button data-id = ${element.idMeal}  id = "recipe" class="btn btn-success">View Recipe</button>
                 <button type="button" id= "like" class="btn btn-outline-danger" > Like! ${EMPTY_HEART} </button> 
             <div>
         `
         let recipeBtn = foodDetails.querySelectorAll("button#recipe")
-
-        recipeBtn.forEach(element => {
+        let recipe = foodDetails.querySelectorAll("div#recipe") 
+        let hideRecipe = foodDetails.querySelectorAll("button#hide-recipe")
+       
+        hideRecipe.forEach(element => {
             element.addEventListener("click",(event) => {
-                console.log("clicked") 
+                //console.log("click")
+                for(let i = 0; i <= recipe.length; i++) {
+                    recipe[i].style.display = "none"
+                }
+
             })
         })
 
+        recipeBtn.forEach(element => {
+            element.addEventListener("click",(event) => {
+                for(let i = 0; i <recipe.length; i++) {
+                    recipe[i].style.display = "block"
+                }
+
+            })
+        })
+ 
         let likeBtn = foodDetails.querySelectorAll("button#like")
 
         likeBtn.forEach(element => {
